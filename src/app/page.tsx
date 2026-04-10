82ffcd83712433d26f9df4ee34162f019c1cbf8b
hirozumit <hirozumi.takeda@monosus.co.jp>
2026-04-10 10:59:50 +0900

    fix text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <p>main</p>
+      <p>trailrun-arcteryx-jp</p>
     </div>
   );
 }
