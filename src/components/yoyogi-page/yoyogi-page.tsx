1026c544af57e371b13e6344a21b12f62b82aa0b
hirozumit <hirozumi.takeda@monosus.co.jp>
2026-04-29 21:49:29 +0900

    revise map layout

--- src/components/yoyogi-page/yoyogi-page.tsx
+++ src/components/yoyogi-page/yoyogi-page.tsx
@@ -440,6 +440,17 @@ export function YoyogiPage() {
             </div>
           </div>
           <div className={styles["facility-detail"]} data-reveal="fade-up">
+            <div className={styles["facility-map"]}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.4345074799626!2d139.69871441254514!3d35.66630173067758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d003c44e355%3A0x4f53a3a729034f15!2sRuntrip%20BASE%20YOYOGI%20PARK!5e0!3m2!1sja!2sjp!4v1777432440197!5m2!1sja!2sjp"
+                width={600}
+                height={450}
+                style={{ border: "0" }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </div>
             <div className={styles["facility-info"]}>
               <p className={styles["facility-name"]}>
                 ARC&apos;TERYX TRAIL HUB YOYOGI
@@ -473,18 +484,7 @@ export function YoyogiPage() {
                   </ul>
                 </NoteToggle>
               </div>
-            </div>
-            <div className={styles["facility-map"]}>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.4345074799626!2d139.69871441254514!3d35.66630173067758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d003c44e355%3A0x4f53a3a729034f15!2sRuntrip%20BASE%20YOYOGI%20PARK!5e0!3m2!1sja!2sjp!4v1777432440197!5m2!1sja!2sjp"
-                width={600}
-                height={450}
-                style={{ border: "0" }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              />
-            </div>
+            </div>            
           </div>
         </div>
 
