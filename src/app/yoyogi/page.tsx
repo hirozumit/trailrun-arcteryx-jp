90f6574c9246a5923c5ed3d4528332f2a430fa05
hirozumit <hirozumi.takeda@monosus.co.jp>
2026-04-30 15:07:20 +0900

    Add OGP metadata and image for Yoyogi page

--- src/app/yoyogi/page.tsx
+++ src/app/yoyogi/page.tsx
@@ -6,10 +6,23 @@ import { Footer } from "@/components/footer/footer";
 import copyLine1 from "@/components/splash/copy-yoyogi-line1.svg";
 import copyLine2 from "@/components/splash/copy-yoyogi-line2.svg";
 
+const title = "TRAIL HUB YOYOGI | ARC'TERYX アークテリクス";
+const description =
+  "山へ行き、山に触れ、何かが変わる。その第一歩を踏み出す前の方や、今まさに踏み出しつつある方へ。アークテリクスによるトレイルランニングのための準備拠点が、代々木公園のほど近くに、期間限定でオープンします。";
+
 export const metadata: Metadata = {
   title: "TRAIL HUB YOYOGI",
-  description:
-    "アークテリクスによるトレイルランニングのための準備拠点が、代々木公園のほど近くに期間限定でオープン。",
+  description,
+  openGraph: {
+    title,
+    description,
+    url: "/yoyogi/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+  },
 };
 
 export default function Yoyogi() {
